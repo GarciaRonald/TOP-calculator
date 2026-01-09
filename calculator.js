6eb09a407803012be46ec3085ec39f2dc2d93ae9
Ronald Garcia <ronaldgm@gmail.com>
2026-01-08 23:06:54 -0500

    Create function to call a math function based on operator

--- calculator.js
+++ calculator.js
@@ -6,3 +6,18 @@ const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
+
+const operate = (num1, op, num2) => {
+    n1 = Number(num1);
+    n2 = Number(num2);
+
+    if (op === '+') {
+        return add(n1, n2);
+    } else if (op === '-') {
+        return subtract(n1, n2);
+    } else if (op === '*') {
+        return multiply(n1, n2);
+    } else if (op === '/') {
+        return divide(n1, n2);
+    }
+};
